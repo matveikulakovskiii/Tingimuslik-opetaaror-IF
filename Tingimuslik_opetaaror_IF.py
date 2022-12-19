26c2b41446610ea4e686763375f0b1f9033710d0
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2022-12-19 21:06:07 +0200

    Update Tingimuslik_opetaaror_IF.py

--- Tingimuslik_opetaaror_IF.py
+++ Tingimuslik_opetaaror_IF.py
@@ -74,3 +74,115 @@
 
 
 
+
+#try:
+#    a=float(input("Mis on teie toatemperatuur: "))
+#    if a>=18:
+#        print("Teie toatemperatuur on keskmisest kõrgem, 18 kraadi Celsiuse järgi.")
+#    else:
+#        print("Teie toatemperatuur on keskmisest madalam, 18 kraadi Celsiuse järgi.")
+#except:
+#    print("Vale Andmetüüp")
+
+
+
+#try:
+#    a=float(input("Mis on su pikkus?(Sentimeetrites): "))
+#    if a<=0:
+#        print("Error")
+#    else:
+#        if a<=165:
+#            print("Teie pikkus on madal")
+#        elif a>165 and a<180:
+#            print("Teie pikkus on keskmine")
+#        elif a>=180:
+#            print("Teie pikkus on kõrge")
+#except:
+#    print("Vale Andmetüüp")
+
+
+
+
+#try:
+#    a=float(input("Mis on su pikkus?(Sentimeetrites): "))
+#    try:
+#        b=int(input("Mis on sinu sugu?(mees-1 või naine-0): "))
+#        if b>1:
+#            print("Error")
+#        else:
+#            if b==1:
+#                if a<=0:
+#                     print("Error")
+#                else:
+#                     if a<170:
+#                         print("Teie pikkus on madal")
+#                     elif a>=170 and a<180:
+#                         print("Teie pikkus on keskmine")
+#                     elif a>=180:
+#                         print("Teie pikkus on kõrge")
+#            else:
+#                if a<=0:
+#                    print("Error")
+#                else:
+#                    if a<160:
+#                        print("Teie pikkus on madal")
+#                    elif a>=160 and a<170:
+#                        print("Teie pikkus on keskmine")
+#                    elif a>=170:
+#                        print("Teie pikkus on kõrge")
+#    except:
+#        print("Vale Andmetüüp")
+#except:
+#    print("Vale Andmetüüp")
+
+
+#try:
+#    a=int(input("Kas soovite piima osta?(jah-1 või ei-0): "))
+#    b=int(input("Kas soovite saia osta?(jah-1 või ei-0): "))
+#    c=int(input("Kas soovite leiba osta?(jah-1 või ei-0): "))
+#    if a==1 and b==0 and c==0:
+#        piim=0.79
+#        sai=0
+#        leib=0
+#        S=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {S}")
+#    elif a==0 and b==1 and c==0:
+#        piim=0
+#        sai=0.8
+#        leib=0
+#        P=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {P}")
+#    elif a==0 and b==0 and c==1:
+#        piim=0
+#        sai=0
+#        leib=0.8
+#        F=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {F}")
+#    elif a==1 and b==1 and c==0:
+#        piim=0.79
+#        sai=0.8
+#        leib=0
+#        L=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {L}")
+#    elif a==0 and b==1 and c==1:
+#        piim=0
+#        sai=0.8
+#        leib=0.8
+#        O=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {O}")
+#    elif a==1 and b==0 and c==1:
+#        piim=0.79
+#        sai=0
+#        leib=0.8
+#        U=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {U}")
+#    elif a==1 and b==1 and c==1:
+#        piim=0.79
+#        sai=0.8
+#        leib=0.8
+#        A=piim+sai+leib
+#        print(f"Kõik ostetud asjad maksavad {A}")
+#except:
+#    print("Vale Andmetüüp")
+
+
